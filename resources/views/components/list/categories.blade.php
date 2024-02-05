8770dc3ff5f23d98220bb6d4df96002d95023490
Dealense <dealense@gmail.com>
2024-02-06 00:28:05 +0400

    fix issues

--- resources/views/components/list/categories.blade.php
+++ resources/views/components/list/categories.blade.php
@@ -1,9 +1,9 @@
-<div class="grid grid-cols-10" x-data="{open: ''}">
+<div class="grid grid-cols-10" x-data="{open: '' }">
     <div class="col-span-10 bg-white shadow p-3 rounded-[4px]">
         <ul class="font-normal  text-xs flex items-center text-gray-700 justify-between">
             @foreach($categories as $category)
                 <li
-                    x-init="{{in_array($category->id, $parentCategoryIds) || !empty(array_intersect($categoryIds, $category->children->pluck('id')->toArray()))}} ? open = '{{$category->slug}}' : ''"
+                    x-init="{{ in_array($category->id, $parentCategoryIds) || !empty(array_intersect($categoryIds, $category->children->pluck('id')->toArray())) ? 'open = \'' . $category->slug . '\'' : '' }}"
                     :class="open === '{{$category->slug}}' ? 'font-bold text-gray-900' : ''"
                     class="cursor-pointer"
                     x-on:click="open === '{{$category->slug}}' ? open = '' : open = '{{$category->slug}}'"
@@ -23,8 +23,7 @@
                     <li class="mr-4 font-normal text-xs">
                         <a
                             href="items?filters[categoryIds][]={{$child->id}}"
-                            class="p-1 hover:bg-gray-200 rounded-[4px] cursor-pointer"
-                            :class="{{in_array($child->id, $categoryIds)}} ? 'bg-gray-200 p-1' : ''"
+                            class="p-1 hover:bg-gray-200 rounded-[4px] cursor-pointer {{ in_array($child->id, $categoryIds) ? 'bg-gray-200 p-1' : '' }}"
                         >
                             {{$child->name}}
                         </a>
